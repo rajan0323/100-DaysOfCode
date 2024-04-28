f463ffb1489983a0cdef47c92ad0b95466d88604
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-04-28 23:17:52 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insert an Element at the Bottom of a Stack/insert-an-element-at-the-bottom-of-a-stack.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    static File file;
+    static BufferedReader in;
+    static PrintWriter out;
+
+    public static void main(String args[]) throws IOException {
+        in=new BufferedReader(new InputStreamReader(System.in));
+        out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine());
+
+        while (t-- > 0) {
+
+            String s[] = in.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int x = Integer.parseInt(s[1]);
+            s = in.readLine().trim().split(" ");
+            Stack<Integer> st = new Stack<>();
+            for (int i = 0; i < n; i++) {
+                st.push(Integer.parseInt(s[i]));
+            }
+            Solution ob = new Solution();
+            Stack<Integer> S = ob.insertAtBottom(st, x);
+            for (int i : S) {
+                out.print(i + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public Stack<Integer> insertAtBottom(Stack<Integer> st, int x) {
+        if(st.isEmpty()){
+            st.push(x);
+            return st ;
+        }
+        int top = st.pop();
+        insertAtBottom(st,x);
+        st.push(top);
+        return st;
+    }
+}
