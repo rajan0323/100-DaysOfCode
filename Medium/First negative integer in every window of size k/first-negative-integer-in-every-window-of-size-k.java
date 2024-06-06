11420071a41b24b83e86bec9942c8f627044521f
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-06-06 23:49:09 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First negative integer in every window of size k/first-negative-integer-in-every-window-of-size-k.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            int k = Integer.parseInt(br.readLine().trim());
+            
+            Compute obj = new Compute();
+            long answer[] = obj.printFirstNegativeInteger(a, n, k);
+            int sz = answer.length;
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<sz;i++)
+                output.append(answer[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public long[] printFirstNegativeInteger(long A[], int N, int K)
+    
+    {
+        long[] result = new long[N - K + 1];
+        for(int i =0;i<N-K+1;i++){
+            for(int j=0;j<K;j++){
+                long ele = A[i+j];
+                if(ele<0){
+                    result[i]=ele;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
