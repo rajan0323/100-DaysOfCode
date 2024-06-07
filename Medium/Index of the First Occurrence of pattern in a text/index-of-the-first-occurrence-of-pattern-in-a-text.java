02d8561ec09373ea35c4d89e58b34b9a14cf1bcb
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-06-07 23:37:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Index of the First Occurrence of pattern in a text/index-of-the-first-occurrence-of-pattern-in-a-text.java
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String text = sc.next();
+            String pat = sc.next();
+            Solution obj = new Solution();
+            int res = obj.findMatching(text, pat);
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findMatching(String text, String pat) {
+     return text.indexOf(pat);
+    }
+}
