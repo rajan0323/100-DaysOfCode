1b2326b3115cc9e17024da73098c033fef38556e
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-06-13 23:55:25 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Leaders in an array/leaders-in-an-array.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter ot = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+
+        while (t-- > 0) {
+
+            // input size of array
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            // inserting elements in the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Solution obj = new Solution();
+
+            StringBuffer str = new StringBuffer();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+
+            // calling leaders() function
+            res = obj.leaders(n, arr);
+
+            for (int i = 0; i < res.size(); i++) {
+                ot.print(res.get(i) + " ");
+            }
+
+            ot.println();
+        }
+        ot.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int n, int arr[]) {
+        ArrayList<Integer> sc = new ArrayList<>();
+        int maxele= arr[n-1];
+        
+        
+        for(int i =n-1;i>=0;i--){
+        
+        
+            if(maxele<=arr[i]){
+                maxele= arr[i];
+                sc.add(maxele);
+                
+                 
+            }
+        }
+        Collections.reverse(sc);
+        return sc;
+    }
+}
