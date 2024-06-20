a7d1ae32e19f3638888c7f61521d91fc455f3751
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-06-20 23:40:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Immediate Smaller Element/immediate-smaller-element.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().immediateSmaller(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void immediateSmaller(int arr[], int n) {
+        
+        int i =1;
+     while(i<=n-1){
+         if(arr[i-1]>arr[i]){
+             arr[i-1]=arr[i];
+             
+         }else{
+             arr[i-1]=-1;
+         }
+         i++;
+        
+     }
+     arr[n-1]=-1;
+    
+    }
+}
