9309a34612c6fa2af93f34377f52b32553f69c7a
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-06-21 15:47:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Set kth bit/set-kth-bit.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+           
+            Solution ob = new Solution();
+            int ans = ob.setKthBit(N,K);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int setKthBit(int N,int K){
+         int bm= 1<<K;
+         int ans = bm | N;
+         return ans;
+    }
+}
