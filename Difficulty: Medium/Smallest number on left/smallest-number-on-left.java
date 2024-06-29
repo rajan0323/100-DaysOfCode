91e882249a67ba3f193d825031c0b81e5c717d6b
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-06-29 15:36:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest number on left/smallest-number-on-left.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int n = Integer.parseInt(in.readLine());
+            String a1[] = in.readLine().trim().split("\\s+");
+            int a[] = new int[n];
+            for(int i = 0;i < n;i++)
+                a[i] = Integer.parseInt(a1[i]);
+            Solution ob = new Solution();
+            List<Integer> ans = new ArrayList<Integer>();
+            ans = ob.leftSmaller(n, a);
+            for(int i = 0;i < n;i++)
+                System.out.print(ans.get(i)+" ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static List<Integer> leftSmaller(int n, int a[])
+    {
+        Stack<Integer> sb = new Stack<>();
+        List<Integer> ans = new ArrayList<>();
+        sb.push(-1);
+        for(int i =0;i<n;i++ ){
+            int curr = a[i];
+            while(sb.peek()>=curr)
+                sb.pop();
+            
+            ans.add(sb.peek());
+            sb.push(curr);
+        }
+        return ans;
+    }
+}
