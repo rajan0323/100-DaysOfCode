28b4a7caae4c35af5aa686de6fb3e07c24fcad17
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-08-17 23:16:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.java
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -1,55 +1,65 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
-import java.util.*; 
+import java.util.*;
 
-class GFG{
-    public static void main(String args[]) throws IOException { 
+class GFG {
+    public static void main(String args[]) throws IOException {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t > 0){
-        	int n = sc.nextInt();
-        	int[] array = new int[n];
-        	for (int i=0; i<n ; i++ ) {
-        		array[i] = sc.nextInt();
-        	}
+        while (t > 0) {
+            int n = sc.nextInt();
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = sc.nextInt();
+            }
             Solution ob = new Solution();
             long[] ans = new long[n];
-            ans = ob.productExceptSelf(array, n); 
+            ans = ob.productExceptSelf(array);
 
-           	for (int i = 0; i < n; i++) { 
-				System.out.print(ans[i]+" ");
-			} 
+            for (int i = 0; i < n; i++) {
+                System.out.print(ans[i] + " ");
+            }
             System.out.println();
             t--;
         }
-    } 
-} 
-  
-
+    }
+}
 
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-
-class Solution 
-{ 
-	public static long[] productExceptSelf(int nums[], int n) 
-	{ 
+class Solution {
+    public static long[] productExceptSelf(int nums[]) {
+        int n = nums.length;
         long[] ans = new long[n];
-        ans[0]= 1;
-        for(int i =1;i<n ;i++){
-           
-            ans[i]= (long)(ans[i-1]* nums[i-1]);
+        
+        // Step 1: Calculate the product of all non-zero elements
+        long totalProduct = 1;
+        int zeroCount = 0;
+        
+        for (int num : nums) {
+            if (num == 0) {
+                zeroCount++;
+            } else {
+                totalProduct *= num;
+            }
         }
-       long  suffix =1;
-        for(int k =n-1;k>=0;k--){
-            ans[k] = (long)(suffix*ans[k]);
-            suffix = (long)(suffix*nums[k]);
+        
+        // Step 2: Fill the answer array based on the number of zeros
+        for (int i = 0; i < n; i++) {
+            if (zeroCount > 1) {
+                ans[i] = 0; // More than one zero means all products are zero
+            } else if (zeroCount == 1) {
+                ans[i] = (nums[i] == 0) ? totalProduct : 0; // Only one zero
+            } else {
+                ans[i] = totalProduct / nums[i]; // No zeros in the array
+            }
         }
-        return ans ;
-	} 
-} 
+        
+        return ans;
+    }
+}
