6d1e38572eff30c551919fd3577a369c66f9f5a0
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-08-22 14:36:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/One's Complement/ones-complement.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.onesComplement(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int onesComplement(int N){
+        int mask = (1<<Integer.toBinaryString(N).length())-1;
+        int ans = mask^N;
+        return ans;
+       
+    }
+}
